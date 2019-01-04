c3bfaac6c07c887313ed9fac8813b57201664e71
federico <171538@umons.ac.be>
2019-01-04 23:06:42 +0100

    Affichage transactions

--- app/src/main/java/example/com/famss10/DatabaseAccess.java
+++ app/src/main/java/example/com/famss10/DatabaseAccess.java
@@ -439,7 +439,7 @@ public class DatabaseAccess {
 
             Cursor c = db.rawQuery("select Transactions.Mountant from " +
                     "Transactions Inner join Diary on (Transactions.idDiary = Diary.idDiary) where " +
-                    "Transactions.idCount = '"+count+"' and Date between '"+start+"' and '"+end+"' and Transactions.Operation = 'dep' or Transactions.Operation = 'tra'", new String[]{});
+                    "Transactions.idCount = '"+count+"' and Date between '"+start+"' and '"+end+"' and Transactions.Operation = 'Type : Dépense' or Transactions.Operation = 'Type : Transfert'", new String[]{});
 
         StringBuffer buffer= new StringBuffer();
         while(c.moveToNext()){
@@ -458,7 +458,7 @@ public class DatabaseAccess {
 
         Cursor c = db.rawQuery("select Transactions.Mountant from " +
                 "Transactions Inner join Diary on (Transactions.idDiary = Diary.idDiary) where " +
-                "Transactions.idCount = '"+count+"' and (Date between '"+start+"' and '"+end+"') and Transactions.Operation = 'rev' ", new String[]{});
+                "Transactions.idCount = '"+count+"' and (Date between '"+start+"' and '"+end+"') and Transactions.Operation = 'Type : Revenu' ", new String[]{});
 
         StringBuffer buffer= new StringBuffer();
         while(c.moveToNext()){
