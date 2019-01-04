690eb5d3894b1428b8417fd74fdc5efd7d3224e4
Harry-N <Harry-N@users.noreply.github.com>
2019-01-04 17:45:29 +0100

    JPP JPP JPP JPP JPP JPP JPP JPP JPP JPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP Normalement ma partie sur count c'est bon => Reste le update et les doubles select à transformer en innerjoin
    Et la date GOOOOOOOO VOIR Registrer les deux fonctions
    
    JPP Harry

--- app/src/main/java/example/com/famss10/ModifyCount.java
+++ app/src/main/java/example/com/famss10/ModifyCount.java
@@ -28,27 +28,36 @@ public class ModifyCount extends AppCompatActivity {
         this.cbMonnaie=findViewById(R.id.checkBoxMonnaie);
 
         tvName.setEnabled(false);
-        tvName.setAlpha(0.0f);
+       // tvName.setAlpha(0.0f);
         tvMontant.setEnabled(false);
-        tvMontant.setAlpha(0.0f);
+       // tvMontant.setAlpha(0.0f);
         tvMonnaie.setEnabled(false);
-        tvMonnaie.setAlpha(0.0f);
+      //  tvMonnaie.setAlpha(0.0f);
 
         if (cbName.isChecked()){
-            tvName.setEnabled(false);
-            tvName.setAlpha(0.0f);
-        }
-        else{tvName.setEnabled(false);
-            tvName.setAlpha(0.0f);}
-        if (cbMontant.isChecked()){
-            tvMontant.setEnabled(false);
-            tvMontant.setAlpha(0.0f);
-        }
-        else { tvMontant.setEnabled(false);
-            tvMontant.setAlpha(0.0f);}
-        if(cbMonnaie.isChecked()){
+            tvName.setEnabled(true);
+            tvName.setAlpha(1.0f);
 
         }
+        else{tvName.setEnabled(false);
+            //tvName.setAlpha(0.0f);
+      }
+        if (cbMontant.isChecked()){
+            tvMontant.setEnabled(true);
+            tvMontant.setAlpha(1.0f);
+
+        }
+        else { tvMontant.setEnabled(false);
+           // tvMontant.setAlpha(0.0f);
+       }
+        if(cbMonnaie.isChecked()){
+            tvMonnaie.setEnabled(true);
+            tvMonnaie.setAlpha(1.0f);
+
+        }
+        else{   tvMonnaie.setEnabled(false);
+           // tvMonnaie.setAlpha(0.0f);
+       }
 
 
 
